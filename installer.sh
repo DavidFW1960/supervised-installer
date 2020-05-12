838b158f9b9f9a0095f3e8b758527e26986eeefd
DavidFW1960 <36390762+DavidFW1960@users.noreply.github.com>
2020-05-12 12:15:50 +1000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -13,7 +13,7 @@ warn "Home Assistant as a VM or run Home Assistant Core"
 warn "via a Docker container."
 warn ""
 echo 'Please typ "not supported" to continue this installation'
-read -t 30 x
+x="not supported" 
 if [ "$x" != "not supported" ]
 then
   echo "OK, bye!"
